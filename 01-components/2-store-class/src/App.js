87e1170ac2bdaf63b6a89a5e05eeacc9b2b05076
Nikita Peplov <npeplov@yandex.ru>
2022-12-14 16:15:55 +0300

    hw #01-2 complete

--- 01-components/2-store-class/src/App.js
+++ 01-components/2-store-class/src/App.js
@@ -1,6 +1,27 @@
 import './styles/App.css';
+import ShopItemClass from './components/ShopItemClass';
 
 function App() {
+  const items = [
+    {
+      id: 1,
+      brand: 'Tiger of Sweden 1',
+      title: 'Leonard coat',
+      description: 'Minimalistic coat in cotton-blend',
+      descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+      price: 399,
+      currency: '£'
+    },
+    {
+      id: 2,
+      brand: 'Tiger of Sweden 2',
+      title: 'Leonard coat',
+      description: 'Minimalistic coat in cotton-blend',
+      descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+      price: 399,
+      currency: '£'
+    },
+  ]
   return (
     <div className="container">
     <div className="background-element">
@@ -9,7 +30,7 @@ function App() {
       <div className='highlight-overlay'></div>
     </div>
     <div className="window">
-      <ShopItemClass item={item} />
+      <ShopItemClass item={items[0]} />
     </div>
   </div>
   );
