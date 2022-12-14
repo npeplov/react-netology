45adb4ecf61f42f231bd035bfac341617c0daf17
Nikita Peplov <npeplov@yandex.ru>
2022-12-14 14:47:51 +0300

    hw #01-2 fixed typo

---
+++ 01-components/2-store-class/src/App.js
@@ -0,0 +1,18 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemClass item={item} />
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ 01-components/2-store-class/src/components/ShopItemClass.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ShopItemClass extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default ShopItemClass;
