c668bf06427630f09cdf075fb8a197249bc4f945
Nikita Peplov <npeplov@yandex.ru>
2022-12-15 07:36:46 +0300

    hw #01-1 through props

--- 01-components/1-store-func/src/components/ShopItemFunc.jsx
+++ 01-components/1-store-func/src/components/ShopItemFunc.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 
-const ShopItemFunc = ({item}) => {
+const ShopItemFunc = (props) => {
+  
   return (
     <div className="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
       <div className="description">
-        {item.descriptionFull}
+        {props.item.descriptionFull}
       </div>
       <div className="highlihgt-windows mobile">
         <div className="highlihgt-overlay"></div>
       </div>
       <div className="divider"></div>
       <div className="purchase-info">
-        <div className="price">{item.currency}{item.price}</div>
+        <div className="price">{props.item.currency}{props.item.price}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
