37cf13c87b3d2225625a5722d364a5f359788949
Nikita Peplov <npeplov@yandex.ru>
2022-12-15 12:43:36 +0300

    hw #01-3 draft: Need refactor to function
    Need to highlight:
    today,
    holidays,
    previos and next months days.
    Need to display week days through .map

--- 01-components/3-calendar/src/components/Calendar.jsx
+++ 01-components/3-calendar/src/components/Calendar.jsx
@@ -1,27 +1,36 @@
 import React from "react";
-import "moment/locale/ru";
 import moment from "moment/moment";
+import "moment/locale/ru";
 
-export default function Calendar({date}) {
-  moment.locale('ru');
-  console.clear();
-  console.log(moment().daysInMonth());
-  console.log(moment());
+export default function Calendar(props) {
+  moment.locale('ru'); 
 
+  const {date} = props;
+  const weekDays = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс' ];
+
+  const start = new Date(date - (date.getDate() - 1) * 60 * 60 * 24 * 1000);
+  const end = moment(date).daysInMonth();
   
+  const startWeekDay = weekDays.indexOf(moment(start).format('dd'));
+  const prevMonthStart = new Date(start - startWeekDay * 60 * 60 * 24 * 1000);
+  const nextMonthStart = new Date(start + end * 60 * 60 * 24 * 1000);
+
+  let counter = prevMonthStart.getDate();
+  let counter2 = 0;
+
   return (
     <div className="ui-datepicker">
       <div className="ui-datepicker-material-header">
-        <div className="ui-datepicker-material-day">{moment().format('dddd')}</div>
+        <div className="ui-datepicker-material-day">{moment(date).format('dddd')}</div>
         <div className="ui-datepicker-material-date">
           <div className="ui-datepicker-material-day-num">{date.getDate()}</div>
-          <div className="ui-datepicker-material-month">{moment().format('MMMM')}</div>
+          <div className="ui-datepicker-material-month">{moment(date).format('MMMM')}</div>
           <div className="ui-datepicker-material-year">{date.getFullYear()}</div>
         </div>
       </div>
     <div className="ui-datepicker-header">
       <div className="ui-datepicker-title">
-        <span className="ui-datepicker-month">{moment().format('MMMM')}</span>&nbsp;<span className="ui-datepicker-year">{date.getFullYear()}</span>
+        <span className="ui-datepicker-month">{moment(date).format('MMMM')}</span>&nbsp;<span className="ui-datepicker-year">{date.getFullYear()}</span>
       </div>
     </div>
     <table className="ui-datepicker-calendar">
@@ -37,29 +46,35 @@ export default function Calendar({date}) {
       </tr>
     </thead>
     <tbody>
-      {/* <tr>
-        <td className="ui-datepicker-other-month">27</td>
-        <td className="ui-datepicker-other-month">28</td>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
-      </tr> */}
-      {/* вывести в цикле от 1 до 31 */}
-      {/* вывести 7 дней */}
       <tr>
-                
+        {weekDays.map((day, ind) => <td>
+          {(day !== moment(start).format('dd') && counter > 7) 
+            ? counter = prevMonthStart.getDate() + ind
+            : counter = start.getDate() + counter2++
+            }
+        </td>)}
+        {/* <td className="ui-datepicker-other-month">27</td>
+        <td className="ui-datepicker-other-month">28</td>
+       */}
       </tr>
-      {/* <tr>
-        <td>6</td>
-        <td>7</td>
-        <td className="ui-datepicker-today">8</td>
-        <td>9</td>
-        <td>10</td>
-        <td>11</td>
-        <td>12</td>
-      </tr> */}
+      <tr>
+        {weekDays.map(day => <td>{++counter}</td>)}
+      </tr>
+      <tr>
+        {weekDays.map(day => <td>{++counter}</td>)}
+        {/* <td className="ui-datepicker-today">8</td> */}
+      </tr>
+      <tr>
+        {weekDays.map(day => <td>{++counter}</td>)}
+      </tr>
+      <tr>
+      {weekDays.map(day => <td>
+          {(counter < end) 
+            ? ++counter
+            : nextMonthStart.getDate()
+          }
+        </td>)}
+      </tr>
     </tbody>
     </table>
     
