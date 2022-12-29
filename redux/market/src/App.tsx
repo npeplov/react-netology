5768732c2a860faf2ebc67fc6a2b018bc9a56a8c
Nikita Peplov <npeplov@yandex.ru>
2022-12-29 13:15:29 +0300

    Redux Market toolkit typescript initial

--- redux/market/src/App.tsx
+++ redux/market/src/App.tsx
@@ -1,7 +1,12 @@
+import { useAppSelector } from "./app/hooks";
+
 const App: React.FC = () => {
+  const test = useAppSelector(state=>state.market.products)
+    
   return (
     <div className="container">
       Working
+      {test[0].title}
     </div>
   );
 };
